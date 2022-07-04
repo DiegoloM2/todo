93f4c8f5208ef5d900d6a2f9fdb6106f4c8871a1
DiegoloM2 <diego.moreno.manes@hotmail.com>
2022-07-04 09:40:44 +0200

    added redux dev tools

--- backend/frontend/src/store/store.jsx
+++ backend/frontend/src/store/store.jsx
@@ -2,12 +2,13 @@ import {createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer.jsx";
 import checkActionType from "../middleware/thunk";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 let configureStore = (preloadedState = {}) => (
     createStore(
       rootReducer(),
       preloadedState,
-      applyMiddleware(checkActionType, logger)
+      composeWithDevTools(applyMiddleware(checkActionType, logger)),
     ));
 
 export default configureStore;
